850df4098389121ae602bc3834a0d9b8ffa75af0
allen <378917466@qq.com>
2017-09-20 11:56:32 +0800

    数据监测系统UI

---
+++ DataMonitor.DQ.UI/Form.cs
@@ -0,0 +1,23 @@
+﻿using DevComponents.DotNetBar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataMonitor.DQ.UI
+{
+    public partial class Form : Office2007RibbonForm
+    {
+        public Form()
+        {
+            InitializeComponent();
+            this.EnableGlass = false;
+            this.Text = System.Configuration.ConfigurationManager.AppSettings["name"];
+        }
+    }
+}
